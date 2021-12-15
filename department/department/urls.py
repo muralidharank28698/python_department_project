29c5a2d884db1eccf2ebd65b1eb5cd1e0e8977ae
Muralidharan <dharanarjun28698@gmail.com>
2021-12-15 15:34:07 +0530

    initial commit

---
+++ department/department/urls.py
@@ -0,0 +1,41 @@
+"""insertlogin URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path("",include("insertlogin.urls")),
+    path('',views.index,name='index'),
+
+    # path('',views.insertrecord,name='insertrecord'),
+    # path('second/',views.second)
+    path('department/',views.department,name='department'),
+    path('about/',views.about,name='about'),
+    path('ece/',views.ece,name='ece'),
+    path('eee/',views.eee,name='eee'),
+    path('cse/',views.cse,name='cse'),
+    path('it/',views.it,name='it'),
+    path('me/',views.me,name='me'),
+    path('ce/',views.ce,name='ce'),
+    path('mathematics/',views.mathematics,name='mathematics'),
+    path('chemistry/',views.chemistry,name='chemistry'),
+
+    # path('about/',views.about,name='about')
+
+
+]
---
+++ department/department/views.py
@@ -0,0 +1,67 @@
+# from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render
+# from django.http import HttpResponse
+from department.models import Register
+from django.contrib import messages
+# from .forms import Formregister
+
+def index(request):
+    return render(request,'index.html')
+
+
+def department(request):
+    # return HttpResponse("<h1>Hello Muralidharan...</h1>")
+    # return render(request,'login.html')
+    if request.method == 'POST':
+        if request.POST.get('uname') and request.POST.get('email') and request.POST.get('password'):
+            saverecord = Register()
+            saverecord.uname = request.POST.get('uname')
+            saverecord.email = request.POST.get('email')
+            saverecord.password = request.POST.get('password')
+            saverecord.save()
+            messages.success(request,'record saved....')
+            return render(request,'./department.html')
+    else:
+            return render(request,'./department.html')
+        
+
+def about(request):
+    return render(request,'./about.html')
+
+
+def ece(request):
+    return render(request,'./ece.html')
+
+def eee(request):
+    return render(request,'./eee.html')
+
+def cse(request):
+    return render(request,'./cse.html')
+
+def it(request):
+    return render(request,'./it.html')
+
+def me(request):
+    return render(request,'./me.html')
+
+def ce(request):
+    return render(request,'./ce.html')
+
+def mathematics(request):
+    return render(request,'./mathematics.html')
+
+def chemistry(request):
+    return render(request,'./chemistry.html')
+# def insertrecord(request):
+#     form = Formregister(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#     context = {'Formregister':Formregister}
+#     return render(request,'login.html',context)
+
+#  def second(request):
+#     return render(request,'./second.html')
+
